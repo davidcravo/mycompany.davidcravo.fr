0be8ba868736e1366de78bc42e3bb21903e50a24
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-10-28 15:16:50 +0100

    Footer: changement de balise

--- components/footer.php
+++ components/footer.php
@@ -1,8 +1,8 @@
-<main class="footer_main">
+<footer class="footer_main">
     <a href="https://davidcravo.fr/" target="_blank">
         <img src="/assets/images/david_cravo_logo.jpeg" class="footer_logo" alt="Logo de David CRAVO">
         David CRAVO
     </a>
     <div class="footer_title">Projet My Company</div>
     <div class="footer_date">2024</div>
-</main>
+</footer>
